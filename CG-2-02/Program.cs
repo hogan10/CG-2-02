2bcf4b8582e8939787a53bf098afd74885651b42
Erin Hogan <hogan10@charter.net>
2018-07-19 23:04:01 -0500

    Homework 2 part 2

---
+++ CG-2-02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CG_2_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program will calculate the area of a rectangle for you");
+            Console.Write("Enter the length of side A: ");
+            string sideA = Console.ReadLine();
+            Console.Write("Enter the length of side B: ");
+            string sideB = Console.ReadLine();
+
+            int sideAnumber = int.Parse(sideA);
+            int sideBnumber = int.Parse(sideB);
+
+            int area = sideAnumber * sideBnumber;
+            Console.WriteLine("The area of your rectangle is:" + area);
+            Console.ReadLine();
+        }
+    }
+}
